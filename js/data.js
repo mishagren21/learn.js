afa3d310a092581f50658e46ee1fa54c724469ed
Oleksandr Bilyi <o.d.bilyi@gmail.com>
2025-11-19 18:28:30 +0100

    moved items form html to data.js

---
+++ js/data.js
@@ -0,0 +1,181 @@
+const items = [
+  {
+    id: 1,
+    img: "./img/automatic.png",
+    name: "S2-3 Автоматический мини",
+    weightKg: "13 кг",
+    motor: {
+      voltage: "180вт",
+      diametr: "от1,5до 25мм",
+    },
+    price: 19990,
+    tags: [
+      {
+        label: "Прес",
+        value: "tag-green",
+      },
+      {
+        label: "Броня",
+        value: "tag-blue",
+      },
+      {
+        label: "Misha",
+        value: "tag-blue",
+      },
+    ],
+  },
+  {
+    id: 2,
+    img: "./img/item-automatic.png",
+    name: "SG-006-1 Станок для разделки кабеля (Автоматический)",
+    weightKg: "13кг",
+    motor: {
+      voltage: "180вт",
+      diametr: "от1,5до 25мм",
+    },
+    price: 39990,
+    discount: 10,
+    tags: [
+      {
+        label: "-10%",
+        value: "tag-red",
+      },
+      {
+        label: "Прес",
+        value: "tag-green",
+      },
+      {
+        label: "Броня",
+        value: "tag-blue",
+      },
+    ],
+  },
+  {
+    id: 3,
+    img: "./img/automatic-blue.png",
+    name: "SG-006-1 Станок для разделки кабеля",
+    weightKg: "13кг",
+    motor: {
+      voltage: "180вт",
+      diametr: "от1,5до 25мм",
+    },
+    price: 29990,
+  },
+
+  {
+    id: 4,
+    img: "./img/item-automatic.png",
+    name: "SG-006-1 Станок для разделки кабеля (Автоматический)",
+    weightKg: "13кг",
+    motor: {
+      voltage: "180вт",
+      diametr: "от1,5до 25мм",
+    },
+    price: 39990,
+    discount: 10,
+    tags: [
+      {
+        label: "-10%",
+        value: "tag-red",
+      },
+      {
+        label: "Прес",
+        value: "tag-green",
+      },
+      {
+        label: "Броня",
+        value: "tag-blue",
+      },
+    ],
+  },
+  {
+    id: 5,
+    img: "./img/automatic-blue.png",
+    name: "SG-006-1 Станок для разделки кабеля",
+    weightKg: "13кг",
+    motor: {
+      voltage: "180вт",
+      diametr: "от1,5до 25мм",
+    },
+    price: 29990,
+  },
+  {
+    id: 6,
+    img: "./img/automatic.png",
+    name: "S2-3 Автоматический мини",
+    weightKg: "13 кг",
+    motor: {
+      voltage: "180вт",
+      diametr: "от1,5до 25мм",
+    },
+    price: 19990,
+    tags: [
+      {
+        label: "Прес",
+        value: "tag-green",
+      },
+      {
+        label: "Броня",
+        value: "tag-blue",
+      },
+    ],
+  },
+  {
+    id: 7,
+    img: "./img/automatic.png",
+    name: "S2-3 Автоматический мини",
+    weightKg: "13 кг",
+    motor: {
+      voltage: "180вт",
+      diametr: "от1,5до 25мм",
+    },
+    price: 19990,
+    tags: [
+      {
+        label: "Прес",
+        value: "tag-green",
+      },
+      {
+        label: "Броня",
+        value: "tag-blue",
+      },
+    ],
+  },
+  {
+    id: 8,
+    img: "./img/item-automatic.png",
+    name: "SG-006-1 Станок для разделки кабеля (Автоматический)",
+    weightKg: "13кг",
+    motor: {
+      voltage: "180вт",
+      diametr: "от1,5до 25мм",
+    },
+    price: 39990,
+    discount: 10,
+    tags: [
+      {
+        label: "-10%",
+        value: "tag-red",
+      },
+      {
+        label: "Прес",
+        value: "tag-green",
+      },
+      {
+        label: "Броня",
+        value: "tag-blue",
+      },
+    ],
+  },
+  {
+    id: 9,
+    img: "./img/automatic-blue.png",
+    name: "SG-006-1 Станок для разделки кабеля",
+    weightKg: "13кг",
+    motor: {
+      voltage: "180вт",
+      diametr: "от1,5до 25мм",
+    },
+    price: 29990,
+  },
+];
